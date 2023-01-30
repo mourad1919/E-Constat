5362a670d785cda52a64ee037af092bded774da7
Issa BEN MANSOUR <issa.benmansour@siga.tn>
2023-01-30 12:45:58 +0100

    add security

---
+++ E-ConstatFrontEnd/src/app/services/auth.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(
+
+  ) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const serverApiUrl = environment.host;
+    if (!request.url || (request.url.startsWith('http') && !(serverApiUrl && request.url.startsWith(serverApiUrl)))) {
+      return next.handle(request);
+    }
+
+    const token: string | null =
+    localStorage.getItem('access_token');
+
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
+    return next.handle(request);
+  }
+}
